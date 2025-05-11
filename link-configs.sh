498db377b63010dfb6a1035fbbe2277fcd3dd061
Welnyr <thekotech@gmail.com>
2025-05-11 17:17:46 +0500

    Add symlink script

--- link-configs.sh
+++ link-configs.sh
@@ -1,21 +1,62 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+BKP_DIR="$SCRIPT_DIR/bkp"
 
-ln -s
+link() {
+  if [ -L "$HOME/$1$2" ]; then
+    echo "Already linked: ~/$1$2"
+    return
+  fi
 
-# link() {
-#   if [ -L "~/$1" ]; then
-#     echo "~/$1 already linked"
-#     return
-#   fi
+  if [ -f "$HOME/$1$2" ]; then
+    read -p "File ~/$1$2 exists. Replace? (Y/n)" REPLACE
 
-#   if [ -f "~/$1" ]; then
-#     ln -s "$SCRIPT_DIR/$1" ~/$1
-#   fi
+    if [[ $REPLACE = "y" || $REPLACE = "Y" || $REPLACE = "" ]]; then
+      ln -sb "$SCRIPT_DIR/$1$2" "$HOME/$1$2" && echo "Linked ~/$1$2"
 
-#   ln -s "$SCRIPT_DIR/$1" ~/$1
-# }
+      if [ ! -d $BKP_DIR ]; then
+        mkdir $BKP_DIR
+      fi
 
-# link ".zshrc"
-# link ".tmux.conf"
+      SUBDIR=$BKP_DIR/$1
+      if [ ! -d $SUBDIR ]; then
+        mkdir -p "$SUBDIR"
+      fi
+
+      mv -b "$HOME/$1$2~" "$BKP_DIR/$1$2"
+    fi
+
+    return
+  fi
+
+  ln -s "$SCRIPT_DIR/$1$2" "$HOME/$1$2" && echo "Linked ~/$1$2"
+}
+
+link "" ".zshrc"
+link "" ".tmux.conf"
+
+link ".config/clipse/" "config.json"
+link ".config/clipse/" "custom_theme.json"
+
+link ".config/dunst/" "dunstrc"
+
+link ".config/fastfetch/" "config.jsonc"
+link ".config/fastfetch/" "arch.txt"
+
+link ".config/fuzzel/" "fuzzel.ini"
+
+link ".config/hypr/" "hyprland.conf"
+link ".config/hypr/" "hyprlock.conf"
+link ".config/hypr/" "hyprpaper.conf"
+link ".config/hypr/" "hyprshot.conf"
+
+link ".config/kitty/" "kitty.conf"
+link ".config/kitty/" "kitty-logo.png"
+
+link ".config/lf/" "colors"
+link ".config/lf/" "icons"
+link ".config/lf/" "lfrc"
+
+link ".config/waybar/" "config.jsonc"
+link ".config/waybar/" "style.css"
