6569f6722a7a68e0ca855ce5f60b0696d97c235e
Welnyr <thekotech@gmail.com>
2025-05-16 01:34:41 +0500

    add zed dots

--- link-configs.sh
+++ link-configs.sh
@@ -5,14 +5,14 @@ BKP_DIR="$SCRIPT_DIR/bkp"
 
 link() {
   if [ -L "$HOME/$1$2" ]; then
-    gum style --foreground 8 "Already linked: ~/$1$2"
+    ln -sf "$SCRIPT_DIR/$1$2" "$HOME/$1$2" && gum style --foreground 2 "~/$1$2  - linked"
     return
   fi
 
   if [ -f "$HOME/$1$2" ]; then
     if gum confirm "File $(gum style --foreground 214 --bold "~/$1$2") exists. Replace?"; then
       ln -sb "$SCRIPT_DIR/$1$2" "$HOME/$1$2"
-      gum style --foreground 2 "Linked ~/$1$2"
+      gum style --foreground 2 "~/$1$2 - linked"
 
       if [ ! -d $BKP_DIR ]; then
         mkdir $BKP_DIR
@@ -26,14 +26,13 @@ link() {
       mv -b "$HOME/$1$2~" "$BKP_DIR/$1$2"
       gum style --foreground 5 "Backup saved to $BKP_DIR/$1$2"
     else
-      gum style --foreground 3 "Skipped ~/$1$2"
+      gum style --foreground 3 "~/$1$2 - skipped"
     fi
 
     return
   fi
 
-  ln -s "$SCRIPT_DIR/$1$2" "$HOME/$1$2"
-  gum style --foreground 2 "Linked ~/$1$2"
+  ln -s "$SCRIPT_DIR/$1$2" "$HOME/$1$2" && gum style --foreground 2 "~/$1$2 - linked"
 }
 
 link "" ".zshrc"
@@ -63,6 +62,10 @@ link ".config/lf/" "lfrc"
 
 link ".config/waybar/" "config.jsonc"
 link ".config/waybar/" "style.css"
+
+link ".config/zed/snippets/" "typescript.json"
+link ".config/zed/" "keymap.json"
+link ".config/zed/" "settings.json"
 
 MONITORS=".config/hypr/monitors.conf"
 if gum confirm "Update monitor configuration?"; then
